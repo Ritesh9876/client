d391b3542bcf03b935b8b21dce8b07c60901c348
Ritesh9876 <riteshmangdare@gmail.com>
2023-10-22 17:54:51 +0530

    add experience section

--- src/components/UserProfile/Components/ProfileEdit/index.js
+++ src/components/UserProfile/Components/ProfileEdit/index.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react'
 import './profileEdit.css'
+import { v4 as uuidv4 } from 'uuid';
 import TextInput from '../../../CommonComponents/TextInput'
 import SelectInputType from '../../../CommonComponents/SelectInput'
+import ButtonComp from '../../../../Utils/Components/ButtonComp';
 function ProfileEdit() {
-  const [experiences,setExperiences]=useState([])
-  
+  const [experiences, setExperiences] = useState([])
+
+  const addNewExperience = () => {
+    let currentExperience = [...experiences]
+    currentExperience.push({
+      role: "",
+      company: "",
+      description: ""
+    })
+    setExperiences(currentExperience)
+  }
+
+  const removeExperience = (index) => {
+    console.log("working ", index)
+    let currentExperience = [...experiences]
+    currentExperience.splice(index, 1)
+    setExperiences(currentExperience)
+  }
   return (
     <div className="profileEdit-container border-ef br-10px pl-30 pt-30 pr-30 pb-30 mt-30 mb-30">
 
@@ -100,6 +118,56 @@ function ProfileEdit() {
           </div>
 
           <div className="profileEdit-right-inner-container">
+
+            <div>
+              {experiences.map((currentExperience, index) => {
+                return (
+                  <div
+                    key={uuidv4()}
+                    className="mt-30">
+
+                    <div>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <p className="font_14_600">Role</p>
+
+                        <div onClick={() => {
+                          removeExperience(index)
+                        }}>
+                          <ButtonComp
+                            status={3} >
+                            Remove
+                          </ButtonComp>
+                        </div>
+                      </div>
+
+                      <div className="mt-10">
+                        <TextInput />
+                      </div>
+                    </div>
+
+                    <div className="mt-10">
+                      <p className="font_14_600">Company Name</p>
+
+                      <div className="mt-10">
+                        <TextInput />
+                      </div>
+                    </div>
+
+                    <div className="mt-10">
+                      <p className="font_14_600">Description</p>
+
+                      <div className="mt-10">
+                        <textarea rows="4" cols="40"></textarea>
+                      </div>
+                    </div>
+
+
+                  </div>
+                )
+              })}
+            </div>
+
+            <p className="link-color pointer-arrow mt-20" onClick={addNewExperience}>add new Experience</p>
 
           </div>
         </div>
